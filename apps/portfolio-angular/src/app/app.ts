138377de605e52c529c561d451ad5ce6c1fa5f72
Kévin MALDONADO <kevin.maldonado@etu.univ-amu.fr>
2025-08-13 15:37:03 +0200

    refactor(navigation): réorganisation du composant navigation

--- apps/portfolio-angular/src/app/app.ts
+++ apps/portfolio-angular/src/app/app.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 // Components
-import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 import { HeroSectionComponent } from './features/hero/hero-section.component';
 import { AboutSectionComponent } from './features/about/about-section.component';
 import { ExperienceSectionComponent } from './features/experience/experience-section.component';
